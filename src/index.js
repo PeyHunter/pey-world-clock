576bcd04f9fd7df3ca3913c6ee3a38b34a349c53
PeyHunter <Nando.goat@gmail.com>
2023-03-22 13:45:14 +0100

    Updated JS
    Today I updated the Js, so the dropDown menu seleced the correct city and displayed that with live time. It was tricky but wonderful when it worked.

--- src/index.js
+++ src/index.js
@@ -1,64 +1,118 @@
-function showSelectedCity (event) {
-    if (event.target.value === "rome") {
-    let romeElement = document.querySelector("#rome")
-    romeElement.innerHTML = "hello"
-    }
+
+  function displayTimes() {
+    // ... your existing code
+    let atlantaDateElement = document.querySelector("#atlantaDate");
+    let atlantaTimeElement = document.querySelector("#atlantaTime");
+    let atlantaDate = moment()
+      .tz("America/New_York")
+      .format(" ddd. Do, MMMM YYYY");
+    let atlantaTime = moment()
+      .tz("America/New_York")
+      .format("hh:mm:ss");
+    let atlantaAM = moment().tz("America/New_York").format("A");
+    atlantaDateElement.innerHTML = `${atlantaDate}`;
+    atlantaTimeElement.innerHTML = `${atlantaTime} <small> ${atlantaAM} </small>`;
+
+    //copenhagen
+    let copenhagenDateElement = document.querySelector("#copenhagenDate");
+    let copenhagenDate = moment().format(" ddd. Do, MMMM YYYY");
+    copenhagenDateElement.innerHTML = `${copenhagenDate}`;
+    let copenhagenAM = moment().tz("Europe/copenhagen").format("A");
+    let copenhagenTimeElement = document.querySelector("#copenhagenTime");
+    let copenhagenTime = moment()
+      .tz("Europe/copenhagen")
+      .format("hh:mm:ss");
+    copenhagenTimeElement.innerHTML = `${copenhagenTime} <small> ${copenhagenAM} </small>`;
+
+    //beijing
+    let beijingDateElement = document.querySelector("#beijingDate");
+    let beijingDate = moment().format(" ddd. Do, MMMM YYYY");
+    beijingDateElement.innerHTML = `${beijingDate}`;
+    let beijingAM = moment().tz("Asia/Hong_Kong").format("A");
+    let beijingTimeElement = document.querySelector("#beijingTime");
+    let beijingTime = moment().tz("Asia/Hong_Kong").format("hh:mm:ss");
+    beijingTimeElement.innerHTML = `${beijingTime} <small> ${beijingAM} </small>`;
+
+    //melbourne
+    let melbourneDateElement = document.querySelector("#melbourneDate");
+    let melbourneDate = moment()
+      .tz("Australia/Melbourne")
+      .format(" ddd. Do, MMMM YYYY");
+    melbourneDateElement.innerHTML = `${melbourneDate}`;
+    let melbourneAM = moment().tz("Australia/Melbourne").format("A");
+    let melbourneTimeElement = document.querySelector("#melbourneTime");
+    let melbourneTime = moment()
+      .tz("Australia/Melbourne")
+      .format("hh:mm:ss");
+    melbourneTimeElement.innerHTML = `${melbourneTime} <small> ${melbourneAM} </small>`;
+
+    // hanoi
+    let hanoiDateElement = document.querySelector("#hanoiDate");
+    let hanoiDate = moment()
+      .tz("Asia/Ho_Chi_Minh")
+      .format(" ddd. Do, MMMM YYYY");
+    hanoiDateElement.innerHTML = `${hanoiDate}`;
+    let hanoiAM = moment().tz("Asia/Ho_Chi_Minh").format("A");
+    let hanoiTimeElement = document.querySelector("#hanoiTime");
+    let hanoiTime = moment().tz("Asia/Ho_Chi_Minh").format("hh:mm:ss");
+    hanoiTimeElement.innerHTML = `${hanoiTime} <small> ${hanoiAM} </small>`;
+  }
+displayTimes();
+setInterval(displayTimes,1000);
+
+
+
+function updateTime() {
+
+
 }
 
+let intervalId;
+function showSelectedCity(event) {
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace(`_`, ``).split("/")[1]
+    let citiesElement = document.querySelector(".cityList");
+    
+    let cityEmojis = {
+        'Rome': '🇮🇹',
+        'Bangkok': '🇹🇭',
+        'Kabul': '🇦🇫',
+        'Berlin': '🇩🇪',
+        'Dakar': '🇸🇳',
+        // Add more cities and their corresponding emojis here
+    };
+    
+    let emoji = cityEmojis[cityName] || '';
+    
+    function updateCityTime () {
+        let cityTime = moment().tz(cityTimeZone)
+        citiesElement.innerHTML = 
+        `<div class="row">
+        <div class="col-6">
+        <h2 class="cityName" >${emoji} ${cityName}</h2>
+        <div class="date" >${cityTime.format(" ddd. Do, MMMM YYYY")}</div>
+        </div>
+        <div class="col-6">
+        <div class="time">${cityTime.format("hh:mm:ss")} <small>${moment().tz(cityTimeZone).format("A")}</small></div>
+        </div>
+        </div>`
+    }
+   updateCityTime();
+    intervalId = setInterval(updateCityTime, 1000);
+}
 let selectDropDown = document.querySelector("#citySelection");
 selectDropDown.addEventListener("change", showSelectedCity)
 
-function displayTimes(){
-setInterval(function displayAtlantaTime () {
-let atlantaDateElement = document.querySelector("#atlantaDate");
-let atlantaTimeElement = document.querySelector("#atlantaTime")
-let atlantaDate = moment().tz("America/New_York").format(" ddd. Do, MMMM YYYY")
-let atlantaTime = moment().tz("America/New_York").format("hh:mm:ss")
-let atlantaAM = moment().tz("America/New_York").format("A")
-
-atlantaDateElement.innerHTML = `${atlantaDate}`;
-atlantaTimeElement.innerHTML = `${atlantaTime} <small> ${atlantaAM} <small/>`
-}, 1000)
-
-setInterval(function displayCopenhagenTime () {
-let copenhagenDateElement = document.querySelector("#copenhagenDate");
-let copenhagenDate = moment().format(" ddd. Do, MMMM YYYY")
-copenhagenDateElement.innerHTML = `${copenhagenDate}`;
-let copenhagenAM = moment().tz("Europe/copenhagen").format("A");
-let copenhagenTimeElement = document.querySelector("#copenhagenTime")
-let copenhagenTime = moment().tz("Europe/copenhagen").format("hh:mm:ss")
-copenhagenTimeElement.innerHTML = `${copenhagenTime} <small> ${copenhagenAM} <small/>`;
-}, 1000)
-
-setInterval(function displayBeijingTime () {
-let beijingDateElement = document.querySelector("#beijingDate");
-let beijingDate = moment().format(" ddd. Do, MMMM YYYY")
-beijingDateElement.innerHTML = `${beijingDate}`;
-let beijingAM = moment().tz("Asia/Hong_Kong").format("A");
-let beijingTimeElement = document.querySelector("#beijingTime")
-let beijingTime = moment().tz("Asia/Hong_Kong").format("hh:mm:ss")
-beijingTimeElement.innerHTML = `${beijingTime} <small> ${beijingAM} <small/>`;
-}, 1000)
-
-setInterval(function displayMelbourneTime () {
-let melbourneDateElement = document.querySelector("#melbourneDate");
-let melbourneDate = moment().tz("Australia/Melbourne").format(" ddd. Do, MMMM YYYY")
-melbourneDateElement.innerHTML = `${melbourneDate}`;
-let melbourneAM = moment().tz("Australia/Melbourne").format("A");
-let melbourneTimeElement = document.querySelector("#melbourneTime")
-let melbourneTime = moment().tz("Australia/Melbourne").format("hh:mm:ss")
-melbourneTimeElement.innerHTML = `${melbourneTime} <small> ${melbourneAM} <small/>`;
-}, 1000)
 
 
-setInterval(function displayHanoiTime () {
-let hanoiDateElement = document.querySelector("#hanoiDate");
-let hanoiDate = moment().tz("Australia/hanoi").format(" ddd. Do, MMMM YYYY")
-hanoiDateElement.innerHTML = `${hanoiDate}`;
-let hanoiAM = moment().tz("Australia/hanoi").format("A");
-let hanoiTimeElement = document.querySelector("#hanoiTime")
-let hanoiTime = moment().tz("Australia/hanoi").format("hh:mm:ss")
-hanoiTimeElement.innerHTML = `${hanoiTime} <small> ${hanoiAM} <small/>`;
-}, 1000)
+
+function reloadPage() {
+    location.reload();
 }
-displayTimes();
+
+let worldClockElement = document.querySelector("#worldClock");
+worldClockElement.addEventListener("click", reloadPage);
+
